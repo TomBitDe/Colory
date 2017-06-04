d4c71cecfa8e83b950e208fca9f179416df6a223
TomBitDe <tbit455674@gmail.com>
2017-06-04 18:36:36 +0200

    Add files via upload

---
+++ ColoryHelpSet/src/main/resources/MakeHelpSearch.bat
@@ -0,0 +1,11 @@
+set PATH=%PATH%;C:\Users\Tom\Entwicklung\Java\jh2.0.05\jh2.0\javahelp\bin
+
+REM cd help
+
+java -jar "C:\Users\Tom\Entwicklung\Java\jh2.0.05\jh2.0\javahelp\bin\jhindexer" -verbose .
+
+jhsearch javahelpsearch
+
+REM cd ..
+
+pause
